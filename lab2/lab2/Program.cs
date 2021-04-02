ea21e6beffbb4442cb2f3db5fb64fa95efa37bf9
s20635 <mikolaj.mialkowski@outlook.com>
2021-04-02 04:20:42 +0200

    IEqualityComparer just implemented

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -1,9 +1,72 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace lab2 {
     class Program {
+        private static string _addressOut;
+        private static string _addressCsv;
+        private static string _fileExtension;
+        
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            if (args.Length != 3) 
+                throw new ArgumentException("Pass correct number of arguments");
+
+            _addressOut = args[0];
+            _addressCsv = args[1];
+            _fileExtension = args[2];
+
+            if (!Directory.Exists(_addressOut))  
+                throw new ArgumentException("Podana ścieżka jest niepoprawna");
+
+            if (!File.Exists(_addressCsv)) { 
+                AddNewErrorToLog("Nie znaleziono pliku CSV");
+                throw new FileNotFoundException("Plik CSV nie istnieje");
+            }
+
+            if (!_fileExtension.Equals("json")) {
+                AddNewErrorToLog("Podano rozszerzenie inne niż JSON");
+                throw new ArgumentException("Json is the only available argument here");
+            }
+            
+            FileInfo fileInfo = new FileInfo(_addressCsv);
+            StreamReader streamReader = new StreamReader(fileInfo.OpenRead());
+            string line;
+            
+            while ((line = streamReader.ReadLine()) != null) {
+                string[] lineAtomValues = line.Split(",");
+
+                if (lineAtomValues.Length == 9) {
+                    if (lineAtomValues.Any(atomValue => atomValue.Equals("")))
+                        AddNewErrorToLog("Rekord zawiera pustą wartość - nie dodano Studenta do wpliku wyjściowego ."+ _fileExtension);
+                    else {
+                        Student student = new Student {
+                            FirstName = lineAtomValues[0],
+                            LastName = lineAtomValues[1],
+                            FieldOfStudy = lineAtomValues[2],
+                            ModeOfStudy = lineAtomValues[3],
+                            IndexNumber = lineAtomValues[4],
+                            BirthDay = lineAtomValues[5],
+                            Email = lineAtomValues[6],
+                            MotherName = lineAtomValues[7],
+                            FatherName = lineAtomValues[8],
+                        };
+                        
+                        
+                    }
+                }
+                else 
+                    AddNewErrorToLog("Zła liczba kolumn");
+                
+            }
+            
+            streamReader.Dispose();
+        }
+
+        private static void AddNewErrorToLog(string errorDescription) {
+             using StreamWriter streamWriter = new (_addressOut+"\\log.txt",true); 
+             streamWriter.WriteLine(errorDescription);
         }
     }
 }
---
+++ lab2/lab2/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab2 {
+    public class Student : IEqualityComparer<Student> {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FieldOfStudy { get; set; }
+        public string ModeOfStudy { get; set; }
+        public string IndexNumber { get; set; }
+        public string BirthDay { get; set; }
+        public string Email { get; set; }
+        public string MotherName { get; set; }
+        public string FatherName { get; set; }
+
+
+        public bool Equals(Student x, Student y) {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            if (x.GetType() != y.GetType()) return false;
+            return x.FirstName == y.FirstName && x.LastName == y.LastName && x.IndexNumber == y.IndexNumber;
+        }
+
+        public int GetHashCode(Student obj) {
+            return HashCode.Combine(obj.FirstName, obj.LastName, obj.IndexNumber);
+        }
+    }
+}
